5f7b705f0f8ef40b06e91a42883a3a74e2e7d598
Ahnaf-Afif <williamhens123@gmail.com>
2026-08-28 11:24:37 +0600

    [client] defer scam library route

--- src/app/scam-library/page.jsx
+++ src/app/scam-library/page.jsx
@@ -1,19 +1,5 @@
-import ScamLibraryHero from "../components/scam-library/ScamLibraryHero";
-import ScamLibraryTabs from "../components/scam-library/ScamLibraryTabs";
-import FeaturedGuide from "../components/scam-library/FeaturedGuide";
-import GuideGrid from "../components/scam-library/GuideGrid";
-import Footer from "../components/shared/Footer";
-import Navbar from "../components/shared/Navbar";
+import { redirect } from "next/navigation";
 
 export default function ScamLibraryPage() {
-  return (
-    <main className="min-h-screen bg-white">
-      <Navbar />
-      <ScamLibraryHero />
-      <ScamLibraryTabs />
-      <FeaturedGuide />
-      <GuideGrid />
-      <Footer />
-    </main>
-  );
+  redirect("/");
 }
